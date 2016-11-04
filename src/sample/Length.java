03530215d98c701e133eafcd2085b437c0eff19c
Jack Powell <jackspider@hotmail.com>
2016-11-04 01:57:47 +0000

    Added several classes for eventual TSP visualisation, separated out length and load from main class.

---
+++ src/sample/Length.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class Length {
+    public static double routeLength(ArrayList<Point2D> cities) {
+
+        //Calculate the length of a TSP route held in an ArrayList as a set of Points
+        double result = 0; //Holds the route length
+        Point2D prev = cities.get(cities.size() - 1);
+
+        //Set the previous city to the last city in the ArrayList as we need to measure the length of the entire loop
+        for (Point2D city : cities) {
+
+            //Go through each city in turn
+            result += city.distance(prev);
+
+            //get distance from the previous city
+            prev = city;
+
+            //current city will be the previous city next time
+        }
+        return result;
+    }
+
+}
