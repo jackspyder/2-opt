fe3c05bdff1c2c5fb36f68cc6001880736d13649
Jack Powell <jackspider@hotmail.com>
2016-11-12 04:24:32 +0000

    added time checks and logging.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -24,28 +24,47 @@ public class Main extends Application {
     public static void main(String[] args) {
         // launch(args);
 
-        ArrayList<Point2D> cities = new ArrayList<>(Load.loadTSPLib("rl100.tsp"));
+        double startTime = System.currentTimeMillis();
+        ArrayList<Point2D> cities = new ArrayList<>(Load.loadTSPLib("rl5915.tsp"));
         ArrayList<Point2D> nearestN;
         ArrayList<Point2D> result;
 
         double length = Length.routeLength(cities);
         System.out.println("Initial tour length is: " + length);
-
+        double time = System.currentTimeMillis() - startTime;
+        System.out.println("Time taken to initialize is: " + time);
         System.out.println("Generating Nearest Neighbour Solution...");
         nearestN = Neighbour.nearest(cities);
         length = Length.routeLength(nearestN);
         System.out.println("Nearest neighbour solution complete, distance: " + length);
         System.out.println("Validating solution...");
         Validator.validate(nearestN);
+        time = System.currentTimeMillis() - startTime;
+        System.out.println("Time taken for init and Nearest Neighbour: " + time);
 
+        startTime = System.currentTimeMillis();
         System.out.println("Attempting 2-opt optimisation...");
         result = TwoOpt.optimise(nearestN);
         length = Length.routeLength(result);
         System.out.println("2-opt solution complete, distance: " + length);
         System.out.println("Validating solution...");
         Validator.validate(result);
+        time = System.currentTimeMillis() - startTime;
+        System.out.println("Time taken for 2 opt optimisation: " + time);
+
+
+        startTime = System.currentTimeMillis();
+        System.out.println("Attempting 2-opt optimisation...");
+        result = TwoOpt.alternate(result);
+        length = Length.routeLength(result);
+        System.out.println("2-opt solution complete, distance: " + length);
+        System.out.println("Validating solution...");
+        Validator.validate(result);
+        time = System.currentTimeMillis() - startTime;
+        System.out.println("Time taken for 2 opt alternate: " + time);
 
         System.out.println("Resulting tour node count: " + result.size());
+
 
 
     }
--- src/sample/TwoOpt.java
+++ src/sample/TwoOpt.java
@@ -8,22 +8,64 @@ public class TwoOpt {
         ArrayList<Point2D> newTour;
         double bestDist = Length.routeLength(cities);
         double newDist;
-        int swaps = 0;
+        int improve = 0;
+        int comparisons = 0;
 
-        for (int i = 0; i < cities.size() - 1; i++) {
-            for (int j = i + 1; j < cities.size(); j++) {
+        for (int i = 1; i < cities.size() - 2; i++) {
+            for (int j = i + 1; j < cities.size() - 1; j++) {
 
                 newTour = swap(cities, i, j);
+                comparisons++;
                 newDist = Length.routeLength(newTour);
                 if (newDist < bestDist) {
                     cities = newTour;
                     bestDist = newDist;
-                    swaps++;
+                    improve++;
+                    System.out.println("Comparisons made: " + comparisons);
+                    System.out.println("Improvements made: " + improve);
+
                 }
             }
         }
 
-        System.out.println("Swaps: " + swaps);
+        System.out.println("Total comparisons made: " + comparisons);
+        System.out.println("Total improvements made: " + improve);
+        return cities;
+    }
+
+    public static ArrayList<Point2D> alternate(ArrayList<Point2D> cities) {
+
+        ArrayList<Point2D> newTour;
+        double bestDist = Length.routeLength(cities);
+        double newDist;
+        int swaps = 1;
+        int improve = 0;
+        int comparisons = 0;
+
+        while (swaps != 0) {
+            swaps = 0;
+            for (int i = 1; i < cities.size() - 2; i++) {
+                for (int j = i + 1; j < cities.size() - 1; j++) {
+                    if ((cities.get(i).distance(cities.get(i - 1)) + cities.get(j + 1).distance(cities.get(j))) >=
+                            (cities.get(i).distance(cities.get(j + 1)) + cities.get(i - 1).distance(cities.get(j)))) {
+
+                        newTour = swap(cities, i, j);
+
+                        newDist = Length.routeLength(newTour);
+                        comparisons++;
+                        if (newDist < bestDist) {
+                            cities = newTour;
+                            bestDist = newDist;
+                            swaps++;
+                            improve++;
+                        }
+
+                    }
+                }
+            }
+        }
+        System.out.println("Total comparisons made: " + comparisons);
+        System.out.println("Total improvements made: " + improve);
         return cities;
     }
 
@@ -33,17 +75,17 @@ public class TwoOpt {
 
         int size = cities.size();
         //
-        for(int c = 0; c <= i-1; c++){
+        for (int c = 0; c <= i - 1; c++) {
             newTour.add(cities.get(c));
         }
 
         int dec = 0;
-        for(int c = i; c<=j; c++){
-            newTour.add(cities.get(j-dec));
+        for (int c = i; c <= j; c++) {
+            newTour.add(cities.get(j - dec));
             dec++;
         }
 
-        for(int c = j+1; c < size; c++){
+        for (int c = j + 1; c < size; c++) {
             newTour.add(cities.get(c));
         }
 
